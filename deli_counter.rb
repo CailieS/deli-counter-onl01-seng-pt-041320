dcf68d22a6ff46c8634312c6bf593a99c5a9a9cd
Cailie Skelton <cskelton707@gmail.com>
2020-04-22 20:04:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
-def line(line)
-    if line.length == 0
+
+def line(katz_deli)
+    if katz_deli.length == 0
       puts "The line is currently empty."
     else
       new_array = []
-      line.each_with_index { |name, i| new_array << "#{i + 1}. #{name}"}
+      katz_deli.each_with_index { |name, i| new_array << "#{i + 1}. #{name}"}
         puts  "The line is currently: " << new_array.join(" ")
     end 
   end 
