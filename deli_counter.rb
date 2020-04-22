bbee415a8a294778145c78fbeb17a76ceb8ba2e9
Cailie Skelton <cskelton707@gmail.com>
2020-04-22 19:54:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 def line(line)
-  if line.length == 0
-  line.each_with_index {|name, i| puts "The line is currently #{i + 1}"}
-  else "The Line is currently empty."
-  end
-end
+    if line.length == 0
+      puts "The line is currently empty."
+    else
+      line.each_with_index { |name, i| name << "The line is currently: #{i + 1} #{name}"}
+    end 
+  end 
 
 def take_a_number
   
